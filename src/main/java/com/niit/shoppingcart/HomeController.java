029e9605a58a60675dde66eba8517cc9e0b8aa39
Sovan Jana <janasovan@gmail.com>
2016-08-20 03:17:40 +0530

    #2 Commit
    20-August-2016

---
+++ src/main/java/com/niit/shoppingcart/HomeController.java
@@ -0,0 +1,45 @@
+package com.niit.shoppingcart;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	
+	/*@RequestMapping
+	public String homePage() {
+		return "Home";
+	}*/
+	
+	@RequestMapping("/")
+	public ModelAndView home() {
+		
+		ModelAndView mv = new ModelAndView("Home");
+		
+		mv.addObject("message", "Thankyou for visiting...");
+		return mv;
+		
+	}
+	
+	@RequestMapping("/Login")
+	public ModelAndView login() {
+		
+		ModelAndView mv = new ModelAndView("Login");
+		
+		mv.addObject("userClickedLoginHere", "true");
+		return mv;
+		
+	}
+	
+	@RequestMapping("/Register")
+	public ModelAndView register() {
+		
+		ModelAndView mv = new ModelAndView("Registration");
+		
+		mv.addObject("userClickedLoginHere", "true");
+		return mv;
+		
+	}
+	
+}
---
+++ src/main/webapp/WEB-INF/views/Home.jsp
@@ -0,0 +1,26 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+	<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+	
+	<c:if test="${#}">
+	<jsp:include page="Login.jsp"></jsp:include>
+	</c:if>
+
+	<c:if test="${#}">
+	<jsp:include page="Registration.jsp"></jsp:include>
+	</c:if>
+	
+	<h2>${message}</h2>
+	<br>
+
+	<jsp:include page="Login.jsp"></jsp:include>
+	<jsp:include page="Registration.jsp"></jsp:include>
+</body>
+</html>
