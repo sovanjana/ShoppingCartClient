cae3bd6809eca88b403627abfa2846823df0f70f
Sovan Jana <janasovan@gmail.com>
2016-08-23 05:19:31 +0530

    #4 Commit
    23-August-2016

--- src/main/java/com/niit/shoppingcart/HomeController.java
+++ src/main/java/com/niit/shoppingcart/HomeController.java
@@ -1,40 +1,36 @@
 package com.niit.shoppingcart;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.niit.shoppingcart.dao.ProductDAO;
+
 @Controller
 public class HomeController {
 	
+	@Autowired
+	ProductDAO productDAO;
+
 	@RequestMapping("/")
-	public ModelAndView home() {
-		
+	public ModelAndView home() {		
 		ModelAndView mv = new ModelAndView("Home");
-		
 		mv.addObject("message", "Thankyou for visiting...");
 		return mv;
-		
 	}
 	
 	@RequestMapping("/Login")
 	public ModelAndView login() {
-		
 		ModelAndView mv = new ModelAndView("Login");
-		
 		mv.addObject("userClickedLoginHere", "true");
 		return mv;
-		
 	}
 	
 	@RequestMapping("/Register")
 	public ModelAndView register() {
-		
 		ModelAndView mv = new ModelAndView("Registration");
-		
 		mv.addObject("userClickedRegisterHere", "true");
 		return mv;
-		
 	}
-	
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,5 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+<h2>Hello from index.jsp . . . .</h2>
 </body>
 </html>
